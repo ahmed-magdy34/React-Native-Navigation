fbbd6daad086d90543944f26089e461ecfb67daf
ahmed-magdy34 <ahmedmagdy334m@gmail.com>
2024-05-06 18:52:29 +0300

    nesting navigators

--- App.js
+++ App.js
@@ -2,9 +2,31 @@ import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './Screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Recipes from './Screens/Recipes';
 import Details from './Screens/Details';
+import FavouriteScreen from './Screens/FavouriteScreen';
+import {Ionicons} from '@expo/vector-icons'
 const Stack=createNativeStackNavigator();
+const Drawer=createDrawerNavigator();
+/////////// Nesting Navigators/////////////
+const DrawerNavigator=()=>{
+  return <Drawer.Navigator screenOptions={{
+    headerStyle:{backgroundColor:'#3F4F4C'},
+    sceneContainerStyle:{backgroundColor:'#EAF8E3'},
+    headerTintColor:'white'
+    }}>
+    <Drawer.Screen name='Categories' component={CategoriesScreen} options={{
+      drawerIcon:({color,size})=><Ionicons color={color} size={size} name='list'/>
+    }}/>
+    <Drawer.Screen name='Favourites' component={FavouriteScreen} options={{
+            drawerIcon:({color,size})=><Ionicons color={color} size={size} name='star'/>
+
+    }}/>
+
+  </Drawer.Navigator>
+}
+/////////////////////////
 
 export default function App() {
   return (
@@ -16,7 +38,9 @@ export default function App() {
         contentStyle:{backgroundColor:'#EAF8E3'},
         headerTintColor:'white'
         }}>
-        <Stack.Screen name='categories' component={CategoriesScreen} />
+        <Stack.Screen name='Drawer' component={DrawerNavigator} options={{
+          headerShown:false
+        }} />
         <Stack.Screen name='Recipes' component={Recipes} />
         <Stack.Screen name='Details' component={Details} />
 
---
+++ Screens/FavouriteScreen.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+const FavouriteScreen=()=>{
+    return(
+        <Text>Favourites</Text>
+    )
+
+}
+export default FavouriteScreen;
